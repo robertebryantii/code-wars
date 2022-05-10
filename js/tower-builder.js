574e0cc5025856cc8264a3c23b2d097fcf624bdc
Robert E Bryant II <robertebryantii@gmail.com>
2022-05-10 19:49:08 -0400

    builds a pyramid of *s inside of an array

---
+++ js/tower-builder.js
@@ -0,0 +1,27 @@
+// Build Tower
+
+// Build a pyramid-shaped tower given a positive integer number of floors. A tower block is represented with "*" character.
+
+// For example, a tower with 3 floors looks like this:
+
+// [
+//   "  *  ",
+//   " *** ", 
+//   "*****"
+// ]
+
+
+function towerBuilder(nFloors) {
+    let space = "";
+    let star = "";
+    let result = [];
+    for (i = 1; i <= nFloors; i++) {
+      space = (" ").repeat(nFloors - i);
+      star = ("*").repeat((2 * i) - 1);
+      result.push(space + star + space);
+    }
+    return result;
+  }
+
+console.log(towerBuilder(3));
+
