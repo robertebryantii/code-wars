a53f3e7b5c21939610db0c3a1e0ef8a8cb718989
Robert E Bryant II <robertebryantii@gmail.com>
2022-05-26 21:24:44 -0400

    regex is confusing

---
+++ js/regex-camelCase.js
@@ -0,0 +1,11 @@
+// Complete the solution so that the function will break up camel casing, using a space between words.
+// Example
+
+// "camelCasing"  =>  "camel Casing"
+// "identifier"   =>  "identifier"
+// ""             =>  ""
+
+function solution(s) {
+  return s.replace(/([A-Z])/g, ' $1')
+}
+
